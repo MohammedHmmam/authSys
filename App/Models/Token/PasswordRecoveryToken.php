2f0ae975a783a5a87f3a3e7d94380843d5644620
Hammam <mohammedhmmam@gmail.com>
2023-03-22 00:37:23 +0200

    Create password recovery generate function

--- App/Models/Token/PasswordRecoveryToken.php
+++ App/Models/Token/PasswordRecoveryToken.php
@@ -4,9 +4,13 @@ namespace App\Models\Token;
 class PasswordRecoveryToken implements IToken{
 
 
+    private static $_tokenText;
+
     public static function generate()
     {
         //Generate password recovery token
+         self::$_tokenText = date('dmth:m:s').'password REcovery' . time() . rand(50,100);
+         return hash("sha1" , self::$_tokenText);
     }
 
 }
