14f24ba1bef533c2fa1749de1e21867bedd17b47
Hammam <mohammedhmmam@gmail.com>
2023-03-22 00:19:32 +0200

    create generate confirmation token method

--- App/Models/Token/ConfirmationToken.php
+++ App/Models/Token/ConfirmationToken.php
@@ -3,9 +3,15 @@ namespace App\Models\Token;
 
 class ConfirmationToken implements IToken{
 
+    private static $_tokenText;
+
     public static function generate()
     {
+
         //Generate Confirmation Token
+        self::$_tokenText = date('dmy h:m:s') . 'confirmEmail'. rand(0,50). time();
+        return hash("sha1",self::$_tokenText);
+        
     }
 
 }
