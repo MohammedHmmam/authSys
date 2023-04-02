e0fbbb1bbebf4f7ce6e11a23a0db3e7bb9537a94
Hammam <mohammedhmmam@gmail.com>
2023-04-02 03:42:57 +0200

    Add bootstrap And fontawseom

---
+++ App/asset/Templates/footer.php
@@ -0,0 +1,4 @@
+<script src="../asset/js/bootstrap.bundle.min.js"></script>
+<script src="../asset/js/all.min.js"></script>
+  </body>
+</html>
---
+++ App/asset/Templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New User</title>
+    <link rel="stylesheet" href="../asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../asset/css/all.min.css">
+    <link rel="stylesheet" href="../asset/css/auth.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&family=Rubik&family=Varela+Round&display=swap" rel="stylesheet">
+
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+   
